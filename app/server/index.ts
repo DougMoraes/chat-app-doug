c169e8b3d8387c92a04676c83b56c18065b35595
DougMoraes <dougmoraes91@gmail.com>
2022-12-13 22:11:45 +0100

    feat: initial implementation of socket

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server } from "socket.io";
 
 const app = express();
+const httpAux = new http.Server(app)
+const socketIO = new Server(httpAux, { cors: { origin: "<http://localhost:3000>" } });
 const PORT = 4000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(cors());
+
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  socket.on('disconnect', () => {
+    socket.disconnect()
+    console.log('🔥: A user disconnected');
+  });
+})
 
 app.get("/api", (req, res) => {
   res.json({
@@ -12,6 +25,6 @@ app.get("/api", (req, res) => {
   })
 });
 
-app.listen(PORT, () => {
+httpAux.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`)
 })
