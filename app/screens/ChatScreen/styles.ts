1ca129181a10f60ba09adc91ec7bc9e6eba1fb63
DougMoraes <dougmoraes91@gmail.com>
2022-12-20 20:12:58 +0100

    feat: create ChatScreen

---
+++ app/screens/ChatScreen/styles.ts
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    chatscreen: {
+        backgroundColor: "#F7F7F7",
+        flex: 1,
+        padding: 10,
+        position: "relative",
+    },
+    chatheading: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "green",
+    },
+    chattopContainer: {
+        backgroundColor: "#F7F7F7",
+        height: 70,
+        width: "100%",
+        padding: 20,
+        justifyContent: "center",
+        marginBottom: 15,
+        elevation: 2,
+    },
+    chatheader: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    chatlistContainer: {
+        paddingHorizontal: 10,
+    },
+    chatemptyContainer: {
+        width: "100%",
+        height: "80%",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    chatemptyText: { fontWeight: "bold", fontSize: 24, paddingBottom: 30 },
+});
