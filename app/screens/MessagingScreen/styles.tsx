aa6d604048635c28aef81988fcba74b35d3402b7
DougMoraes <dougmoraes91@gmail.com>
2023-01-02 14:38:00 +0100

    feat: create MessagingScreen

---
+++ app/screens/MessagingScreen/styles.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    messagingscreen: {
+        flex: 1,
+    },
+    messaginginputContainer: {
+        width: "100%",
+        minHeight: 100,
+        backgroundColor: "white",
+        paddingVertical: 30,
+        paddingHorizontal: 15,
+        justifyContent: "center",
+        flexDirection: "row",
+    },
+    messaginginput: {
+        borderWidth: 1,
+        padding: 15,
+        flex: 1,
+        marginRight: 10,
+        borderRadius: 20,
+    },
+    messagingbuttonContainer: {
+        width: "30%",
+        backgroundColor: "green",
+        borderRadius: 3,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    mmessageWrapper: {
+        width: "100%",
+        alignItems: "flex-start",
+        marginBottom: 15,
+    },
+    mmessage: {
+        maxWidth: "50%",
+        backgroundColor: "#f5ccc2",
+        padding: 15,
+        borderRadius: 10,
+        marginBottom: 2,
+    },
+    mvatar: {
+        marginRight: 5,
+    },
+});
---
+++ app/screens/MessagingScreen/types.tsx
@@ -0,0 +1,4 @@
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from "../../types";
+
+export type messagingScreenProps = NativeStackScreenProps<RootStackParamList, 'Messaging'>;
